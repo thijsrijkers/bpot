8998a332163b7f7823dee809b16fa249dfb4561f
thijsrijkers <thijsrijkers@live.nl>
2021-10-07 15:03:15 +0200

    + Converted project to F# and made a check for your IP address

---
+++ Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    Console.ForegroundColor <- ConsoleColor.White
+    printfn"______       ______     _   
+| ___ \      | ___ \   | |  
+| |_/ /______| |_/ /__ | |_ 
+| ___ \______|  __/ _ \| __|
+| |_/ /      | | | (_) | |_ 
+\____/       \_|  \___/ \__|
+                            "
+    Console.WriteLine client.IP
+    0 //return
---
+++ client.fs
@@ -0,0 +1,20 @@
+﻿module client
+
+open System
+open System.Net
+
+let IP =
+    Console.ForegroundColor <- ConsoleColor.Gray
+    printfn "Getting hostname....."
+    let localHost = Dns.GetHostName()
+    Console.WriteLine localHost
+
+    printfn "Searching for local IP:"
+    Console.ForegroundColor <- ConsoleColor.DarkCyan
+    let ip = Dns.GetHostEntry(localHost).AddressList
+             |> Array.find (fun x -> x.AddressFamily.ToString() = "InterNetwork")
+             |> printfn "Local IP: %A"
+    0
+
+Console.ForegroundColor <- ConsoleColor.Gray
+Console.ReadKey(true) |> ignore
